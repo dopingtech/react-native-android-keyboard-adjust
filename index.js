9166aa7c2561b62248a50361cbe9ea923de2b291
G.Burak <webyonet@hotmail.com>
2023-07-22 10:20:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 import { NativeModules } from 'react-native';
-var AndroidKeyboardAdjust = NativeModules.AndroidKeyboardAdjust;
-module.exports = AndroidKeyboardAdjust;
+
+export type AndroidKeyboardAdjustType = {
+    setAdjustNothing(): void,
+    setAdjustPan(): void,
+    setAdjustResize(): void,
+    setAdjustUnspecified(): void,
+    setAlwaysVisible(): void,
+    setAlwaysHidden(): void,
+    setVisible(): void,
+    setHidden(): void,
+    setUnchanged(): void,
+}
+
+const AndroidKeyboardAdjust: AndroidKeyboardAdjustType = NativeModules.AndroidKeyboardAdjust;
+
+export default AndroidKeyboardAdjust;
